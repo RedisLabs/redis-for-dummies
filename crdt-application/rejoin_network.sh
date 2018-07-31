d97abb93e36b05c5d03c7567cc90409650870db2
Kyle J. Davis <halldirector@gmail.com>
2018-07-31 09:58:11 -0600

    added split/rejoin scripts

---
+++ crdt-application/rejoin_network.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker network connect network2 rp1
+docker network connect network1 rp2
---
+++ crdt-application/split_network.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker network disconnect network2 rp1
+docker network disconnect network1 rp2
